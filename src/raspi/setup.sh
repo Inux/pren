adf5e684fa2d063cc044161c8d2cb550c68129f4
inux <inux.steve@gmail.com>
2018-11-13 23:57:24 +0100

    ui for direction done

--- src/raspi/setup.sh
+++ src/raspi/setup.sh
@@ -1,7 +1,9 @@
 #Raspi Setup
 #
 #1. install raspbian -> https://www.raspberrypi.org/downloads/raspbian/
-#2. sudo apt-get install python3-pip
+#2. packages:
+#   -sudo apt-get install python3-pip // Python3 and pip3
+#   -sudo apt-get install lsof // used to find open ports (kill.sh)
 #3. wifi hotspot -> https://github.com/billz/raspap-webgui
 #       - ssid: RaspiTeam28
 #       - ip: 10.3.141.1
