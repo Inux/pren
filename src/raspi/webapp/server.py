7d02ac1324207d0d419637dba58d52af207b0272
Habbasch <fish.grepper@gmail.com>
2018-11-23 14:39:32 +0100

    Added testing interface for Train movent and position

--- src/raspi/webapp/server.py
+++ src/raspi/webapp/server.py
@@ -25,11 +25,29 @@ async def favicon(request):
 async def api(request):
     return json({'direction': 'right'})
 
+
 @app.route('/sound/<sound_nr>')
 async def play_sound(request, sound_nr):
     print('playing sound: ' + sound_nr)
     sound.Sound.play_sound_by_number(sound_nr)
-    return json('test')
+    return json({'received': True})
+
+@app.route('/simulation/set', methods=["POST",])
+async def simulationSet(request):
+    print('simulating')
+    print(request.json)
+
+    return json({'received': True})
+
+@app.route('/simulation/get')
+async def simulationGet(request):
+    print('getting sim data')
+
+    mov_dict = {}
+    mov_dict['acc'] = 20
+    mov_dict['speed'] = 20
+    mov_dict['distance'] = 20
+    return json(mov_dict)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=2828)
