8bc099834a76d2dab948c2ee99da5e6567ec22a9
inux <inux.steve@gmail.com>
2019-03-14 10:25:28 +0100

    add controlflow and linedetector

--- src/raspi/linedetector/linedetector.py
+++ src/raspi/linedetector/linedetector.py
@@ -16,17 +16,17 @@ from lib import heartbeat as hb
 socket = zmq_socket.make_socket()
 
 def send_hb():
-    hb.send_heartbeat(socket, hb.COMPONENT_CONTROLFLOW, hb.STATUS_RUNNING)
+    hb.send_heartbeat(socket, hb.COMPONENT_LINEDETECTION, hb.STATUS_RUNNING)
 
-class Controlflow(base_app.App):
+class Linedetection(base_app.App):
     def __init__(self, *args, **kwargs):
-        super().__init__("Controlflow", self.controlflow_loop, *args, **kwargs)
+        super().__init__("Linedetection", self.linedetection_loop, *args, **kwargs)
 
         self.job = periodic_job.PeriodicJob(interval=timedelta(milliseconds=50), execute=send_hb)
         self.job.start()
 
-    def controlflow_loop(self, *args, **kwargs):
+    def linedetection_loop(self, *args, **kwargs):
         time.sleep(5)
 
 if __name__ == '__main__':
-    Controlflow().run()
+    Linedetection().run()
