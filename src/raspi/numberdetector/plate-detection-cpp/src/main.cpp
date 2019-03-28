d8b65ad006cf70481cf8d9770e5c4bac1b6dcbba
Patrick Marty <marty.ruthi@outlook.com>
2019-03-28 14:37:06 +0100

    CreateBasic Application OpenCV

--- src/raspi/numberdetector/plate-detection-cpp/src/main.cpp
+++ src/raspi/numberdetector/plate-detection-cpp/src/main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include "PlateDetec.hpp"
+#include "numberReco.pb.h"
+#include <zmq.hpp>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
-	
+    PlateDetec plateDetec;
+    plateDetec.enableWindow();
+    plateDetec.showImage();
+
 }
