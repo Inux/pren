ef756023bd2eb9a8b0b719aa50db55674623b942
inux <inux.steve@gmail.com>
2019-03-29 10:57:56 +0100

    recv handler

--- src/raspi/movement/protocol.py
+++ src/raspi/movement/protocol.py
@@ -17,6 +17,15 @@ class Protocol():
         self.conn = None
         self.ack_map = {}
 
+        #Maps the received message to the function which either
+        #sets the internal value or does a handling with the received value directly
+        self.recv_map = {
+            Message.IS_SPEED : self.__set_recv_speed,
+            Message.CUBE : self.__set_recv_cube,
+            Message.CURRENT : self.__set_recv_current,
+            Message.LOG : self.__set_recv_log
+        }
+
     def connect(self):
         '''
         connects to the serial port
@@ -61,3 +70,35 @@ class Protocol():
         if self.conn is not None:
             self.conn.write(str(message.value)+","+str(value)+"\n")
             self.ack_map[message.value] = time.time()
+
+    def receive_handler(self):
+        '''
+        handles the received lines
+        '''
+        if self.conn is not None:
+            line = self.conn.readline(timeout=1)
+            if line != "" and line is not None:
+                self.__parse_line(line)
+
+    def __parse_line(self, line):
+        try:
+            l = line.split(',')
+            msg = l[0]
+            val = l[1]
+        except Exception as e:
+
+
+    def __set_recv_speed(self, val):
+        pass
+
+    def __set_recv_cube(self, val):
+        pass
+
+    def __set_recv_current(self, val):
+        pass
+
+    def __set_recv_log(self, val):
+        pass
+
+    def __set_recv_ack(self, val):
+        pass
