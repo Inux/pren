221fa69dd3f261b900322080069862ff379c3ff7
Patrick Marty <marty.ruthi@outlook.com>
2019-04-13 11:40:34 +0200

    prepare for dynamic testing

--- src/raspi/numberdetector/numberDetectionPython/platedetection.py
+++ src/raspi/numberdetector/numberDetectionPython/platedetection.py
@@ -1,6 +1,9 @@
 
 import cv2
-from numberReco import NumberReco
+import numpy as np
+#from numberReco import NumberReco
+#import pydevd_pycharm
+#pydevd_pycharm.settrace('192.168.0.31', port=3265, stdoutToServer=True, stderrToServer=True)
 
 
 
@@ -14,7 +17,7 @@ class PlateDetection:
 
 
     def __init__(self):
-        self.nRec = NumberReco()
+        #self.nRec = NumberReco()
         self.cap = cv2.VideoCapture(-1)
         self.windowName = "workwindow"
         self.thresTrackbar = "ThresTrackbar"
@@ -25,9 +28,6 @@ class PlateDetection:
         self.thresFix = 1
         self.hmaxFix = 77
         self.hminFix = 58
-
-
-
 
     def initCam(self):
         w = self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)
@@ -47,7 +47,7 @@ class PlateDetection:
         print("Brigh: " + str(b))
 
     def createTrackbar(self,imgWindow):
-        cv2.createTrackbar(self.thresTrackbar, imgWindow, 0, 150, nothing)
+        cv2.createTrackbar(self.thresTrackbar, imgWindow, 0, 255, nothing)
         cv2.createTrackbar(self.hmaxTrackbar, imgWindow, 0, 150, nothing)
         cv2.createTrackbar(self.hminTrackbar, imgWindow, 0, 150, nothing)
         thres = cv2.getTrackbarPos(self.thresTrackbar, self.windowName)
@@ -66,6 +66,12 @@ class PlateDetection:
         hmin = cv2.getTrackbarPos(self.hminTrackbar, self.windowName)
         hmax = cv2.getTrackbarPos(self.hmaxTrackbar, self.windowName)
 
+        cv2.createTrackbar('iterator', self.windowName, 1, 10, nothing)
+        cv2.createTrackbar('kernel', self.windowName, 1, 30, nothing)
+
+        iterator = cv2.getTrackbarPos('iterator', self.windowName)
+        kernelI = cv2.getTrackbarPos('kernel', self.windowName)
+
 
         while(True):
 
@@ -73,8 +79,21 @@ class PlateDetection:
             ret, frame = self.cap.read()
             orignFram = frame
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            ret, thresh1 = cv2.threshold(gray,thres,255,cv2.THRESH_BINARY_INV)
+            ret, thresh1 = cv2.threshold(gray,thres,255,cv2.THRESH_TOZERO)
+
+            kernel1 = np.ones((kernelI, kernelI), np.uint8)
+
+
+
+            thresh1 = cv2.dilate(thresh1, kernel1, iterations=iterator)
+
+            iterator = cv2.getTrackbarPos('iterator', self.windowName)
+            kernelI = cv2.getTrackbarPos('kernel', self.windowName)
+
+
             contours, hierarchy = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+
 
             try:
 
@@ -83,7 +102,7 @@ class PlateDetection:
                     if ((h > hmin) and (h < hmax)):
                         cv2.rectangle(orignFram, (x, y), (x + w, y + h), (0, 255, 0), 2)
                         rect = [x,y,w,h]
-                        self.nRec.recoNumber(thresh1,rect)
+                        #self.nRec.recoNumber(thresh1,rect)
             except:
                 pass
 
