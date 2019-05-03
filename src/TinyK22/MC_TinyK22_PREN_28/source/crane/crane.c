e18dd531ab1c5ddd96858cd529912e038aebc6ce
Andi-Sail <andreas.rebsamen@bluewin.ch>
2019-05-03 08:36:59 +0200

    add timeout for crane (5 sec) and prevent double rectract

--- src/TinyK22/MC_TinyK22_PREN_28/source/crane/crane.c
+++ src/TinyK22/MC_TinyK22_PREN_28/source/crane/crane.c
@@ -7,6 +7,7 @@
 
 #include "com.h"
 #include "pi.h"
+#include "comLog.h"
 #include "comAck.h"
 #include "McuUtility.h"
 #include "string.h"
@@ -15,6 +16,7 @@
 #include "peripherals.h"
 #include "crane.h"
 #include "quad.h"
+#include <stdbool.h>
 
 #define MOTOR_S_MAX_VALUE   (100000/4)
 
@@ -23,6 +25,9 @@
 #define TICKS_TO_DRIVE      (5435-25)
                             //(TICK_PER_REV*ANGLE_TO_DRIVE/360)
 
+/* 5 sec */
+#define TIMEOUT_VALUE       (5000/25)
+
 static tAckHandler craneAckHandler;
 static tframeLineHandler craneFrameHandler;
 
@@ -30,6 +35,9 @@ static uint32_t targetPos;
 static uint16_t kp, ki, kd;
 static uint32_t val;
 
+static bool craneRetracting = false;
+static bool craneDoneRetracted = false;
+static int timeoutCounter = 0;
 
 static void motor_A_UpdatePwmDutyCycle(uint32_t value)
 {
@@ -46,6 +54,14 @@ static void motor_A_UpdatePwmDutyCycle(uint32_t value)
   FTM_SetSoftwareTrigger(FTM_1_MOTOR_PWM_PERIPHERAL, true);
 }
 
+static void doneRetractCrane(void)
+{
+  //todo send to raspi
+  craneRetracting = false;
+  craneDoneRetracted = true;
+}
+
+
 void craneDoWork(void)
 {
   static int16_t truePos = 0;
@@ -57,81 +73,110 @@ void craneDoWork(void)
   int ctrl_d;
   int currentError;
   //todo check for timeout and let it be if timeout expires
-  truePos = Encoder_S_GetNbrOfTicks();
 
-  if (truePos >= targetPos)
+  if (craneRetracting && timeoutCounter < TIMEOUT_VALUE)
   {
-    //destination reached
-    //reset controler to original state
-    motor_A_UpdatePwmDutyCycle(0);
-    targetPos = 0;
-    setPos = 0;
-    oldError = 0;
-    intError = 0;
-    ctrl_i = 0;
-    return; //todo this is not nice
-  }
+    timeoutCounter++;
+    if (timeoutCounter >= TIMEOUT_VALUE)
+    {
+      //timeout reached
+      //reset controler to original state
+      motor_A_UpdatePwmDutyCycle(0);
+      targetPos = 0;
+      setPos = 0;
+      oldError = 0;
+      intError = 0;
+      ctrl_i = 0;
+      LOG_WARN("crane timeout occurred");
+      doneRetractCrane();
+      return; //todo this is not nice
+    }
 
-  if (targetPos > setPos)
-  {
-    setPos += 40;
-    if (targetPos < setPos)
-      setPos = targetPos;
-  }
+    truePos = Encoder_S_GetNbrOfTicks();
 
-  if (setPos < truePos)
-  {
-    currentError = 0;
-    intError = 0;
+    if (truePos >= targetPos)
+    {
+      //destination reached
+      //reset controler to original state
+      motor_A_UpdatePwmDutyCycle(0);
+      targetPos = 0;
+      setPos = 0;
+      oldError = 0;
+      intError = 0;
+      ctrl_i = 0;
+      doneRetractCrane();
+      return; //todo this is not nice, again
+    }
+
+    if (targetPos > setPos)
+    {
+      setPos += 40;
+      if (targetPos < setPos)
+        setPos = targetPos;
+    }
+
+    if (setPos < truePos)
+    {
+      currentError = 0;
+      intError = 0;
+    }
+    else
+    {
+      currentError = (setPos - truePos);
+    }
+
+    // calculate Controller
+    ctrl_p = kp * currentError;
+    intError += currentError;
+    ctrl_i = ki * intError;
+    ctrl_d = kd * (currentError - oldError);
+    oldError = currentError;
+
+    val = ctrl_p + ctrl_i + ctrl_d;
+
+    val /= 1; //scaling done by widening range of SetPwm
+
+    if (val > MOTOR_S_MAX_VALUE)
+    {
+      val = MOTOR_S_MAX_VALUE;
+      intError -= currentError;
+    }
+    else if (val < 0)
+    {
+      val = 0;
+      intError += currentError;
+    }
+
+    motor_A_UpdatePwmDutyCycle(val);
+
+#if TEST
+    piWriteNum32s("motor_S_SetValue", val, NULL); //todo clean
+#endif
   }
   else
   {
-    currentError = (setPos-truePos);
+    motor_A_UpdatePwmDutyCycle(0);
   }
-
-  // calculate Controller
-  ctrl_p = kp*currentError;
-  intError += currentError;
-  ctrl_i = ki*intError;
-  ctrl_d = kd*(currentError - oldError);
-  oldError = currentError;
-
-  val = ctrl_p + ctrl_i + ctrl_d;
-
-  val /= 1; //scaling done by widening range of SetPwm
-
-  if (val > MOTOR_S_MAX_VALUE)
-  {
-    val = MOTOR_S_MAX_VALUE;
-    intError -= currentError;
-  }
-  else if  (val < 0)
-  {
-    val = 0;
-    intError += currentError;
-  }
-
-
-  motor_A_UpdatePwmDutyCycle(val);
-
-#if TEST
-  piWriteNum32s("motor_S_SetValue", val, NULL); //todo clean
-#endif
 }
 
-tError craneFrameLineHandler(unsigned char *value)
+tError craneFrameLineHandler(const unsigned char *value)
 {
   // todo implement some stuff here
   // todo implement the correct stuff here
-  uint32_t motVal = 0;
-  McuUtility_ScanDecimal32uNumber(&value, &motVal);
+  uint32_t iVal = 0;
+  McuUtility_ScanDecimal32uNumber(&value, &iVal);
 
-  if (motVal > MOTOR_S_MAX_VALUE) motVal = MOTOR_S_MAX_VALUE;
-
-  //motor_A_UpdatePwmDutyCycle(motVal);
-  Encoder_S_StartCountingTicks();
-  targetPos = TICKS_TO_DRIVE;
-
+  if (iVal == 1 && !craneDoneRetracted && !craneRetracting)
+  {
+    Encoder_S_StartCountingTicks();
+    targetPos = TICKS_TO_DRIVE;
+    craneRetracting = true;
+  }
+  else if (iVal == 42 && !craneRetracting && craneDoneRetracted)
+  {
+    craneDoneRetracted = false;
+    timeoutCounter = 0;
+  }
 
   ackSend(&craneAckHandler);
 
