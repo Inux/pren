dc637ca14a84dd9c7e0e9389c7c522d28ebfcacb
inux <inux.steve@gmail.com>
2019-05-09 20:37:15 +0200

    run script

--- src/raspi/run_master.sh
+++ src/raspi/run_master.sh
@@ -1,10 +1,34 @@
 #Run Raspi Applications
 echo "Starting Raspi Applications..."
 
+trap 'killall' INT
+
+killall() {
+    trap '' INT TERM
+    echo "**** Shutting down... ****"
+    kill -TERM 0
+    wait
+    echo DONE
+}
+
+#acoustic
+echo "...Acoustic..."
+python3 acoustic/sound.py &
+
 #linedetector
 echo "...LineDetector..."
 python3 linedetector/fakelinedetector.py &
 
+#movement
+echo "...Movement..."
+python3 movement/movement.py &
+
+#controlflow
+echo "...ControlFlow..."
+python3 controlflow/controlflow.py &
+
 #webapp
 echo "...WebApp..."
 python3 webapp/server.py &
+
+cat # wait forever
