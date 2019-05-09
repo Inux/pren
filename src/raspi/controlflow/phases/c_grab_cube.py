37378a2f88e3a87828dd964b46e7775616c8e13a
inux <inux.steve@gmail.com>
2019-05-09 20:43:06 +0200

    add some time for methods of phases

--- src/raspi/controlflow/phases/c_grab_cube.py
+++ src/raspi/controlflow/phases/c_grab_cube.py
@@ -1,3 +1,5 @@
+import time
+
 count = 0
 limit = 5
 
@@ -5,6 +7,7 @@ def method(middleware_data):
     global count
     if count < limit:
         count = count + 1
+        time.sleep(1)
         return "'" + str(count) + "' < '" + str(limit) + "'"
 
     count = 0
