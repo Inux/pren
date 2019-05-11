cb617143cb98cea43d6d0fff2c2314f97d9767df
Patrick Marty <marty.ruthi@outlook.com>
2019-05-11 12:33:17 +0200

    add movieCatcher & reco Start

--- src/raspi/numberdetector/numberDetectionPython/platedetection.py
+++ src/raspi/numberdetector/numberDetectionPython/platedetection.py
@@ -1,8 +1,7 @@
 
 import cv2
 import numpy as np
-from queue import Queue
-from threading import Thread
+
 # import pydevd_pycharm
 # pydevd_pycharm.settrace('192.168.0.31', port=3265, stdoutToServer=True, stderrToServer=True)
 
@@ -14,9 +13,7 @@ def nothing(x):
 class PlateDetection:
 
     def __init__(self):
-        self.cap = cv2.VideoCapture(0)
-        self.camQueue = Queue(maxsize=10)
-        print(str(self.camQueue.empty))
+        self.cap = cv2.VideoCapture(-1)
 
         self.windowTrackbar = "trackbarWindow"
         self.windowWorkFrame = "workFrameWindow"
@@ -31,32 +28,50 @@ class PlateDetection:
     def initCam(self):
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-        self.cap.set(cv2.CAP_PROP_BRIGHTNESS, 0.5)
-        self.cap.set(cv2.CAP_PROP_CONTRAST, 0.5)
-        self.cap.set(cv2.CAP_PROP_SATURATION, 0.5)
+        self.cap.set(cv2.CAP_PROP_BRIGHTNESS, 70)
+        self.cap.set(cv2.CAP_PROP_CONTRAST, 30)
+        self.cap.set(cv2.CAP_PROP_SATURATION, 30)
+        self.cap.set(cv2.CAP_PROP_WB_TEMPERATURE, 45)
 
     def createTrackbar(self):
         cv2.namedWindow(self.windowTrackbar)
         cv2.createTrackbar(self.threshTrackbar, self.windowTrackbar, 0, 255, nothing)
         cv2.createTrackbar(self.hMaxTrackbar, self.windowTrackbar, 0, 150, nothing)
         cv2.createTrackbar(self.hMinTrackbar, self.windowTrackbar, 0, 150, nothing)
-        cv2.createTrackbar(self.iteratorTrackbar, self.windowTrackbar, 0, 10, nothing)
-        cv2.createTrackbar(self.kernelTrackbar, self.windowTrackbar, 0, 30, nothing)
+        cv2.createTrackbar(self.iteratorTrackbar, self.windowTrackbar, 0, 255, nothing)
+        cv2.createTrackbar(self.kernelTrackbar, self.windowTrackbar, 0, 255, nothing)
 
     def getTrackbarValues(self, trackbar):
         return{
-            self.threshTrackbar: cv2.getTrackbarPos(self.threshTrackbar,self.windowTrackbar),
-            self.hMaxTrackbar: cv2.getTrackbarPos(self.threshTrackbar,self.windowTrackbar),
-            self.hMinTrackbar: cv2.getTrackbarPos(self.threshTrackbar,self.windowTrackbar),
-            self.iteratorTrackbar: cv2.getTrackbarPos(self.threshTrackbar,self.windowTrackbar),
-            self.kernelTrackbar: cv2.getTrackbarPos(self.threshTrackbar,self.windowTrackbar)
-        }.get(trackbar,0)
+            self.threshTrackbar: cv2.getTrackbarPos(self.threshTrackbar, self.windowTrackbar),
+            self.hMaxTrackbar: cv2.getTrackbarPos(self.hMaxTrackbar, self.windowTrackbar),
+            self.hMinTrackbar: cv2.getTrackbarPos(self.hMinTrackbar, self.windowTrackbar),
+            self.iteratorTrackbar: cv2.getTrackbarPos(self.iteratorTrackbar, self.windowTrackbar),
+            self.kernelTrackbar: cv2.getTrackbarPos(self.kernelTrackbar, self.windowTrackbar)
+        }.get(trackbar, 0)
 
-    def filterAndMorphOP(self, frame, threshold, kernel, iterator):
-        kernel1 = np.ones((kernel, kernel), np.uint8)
+    def filterAndMorphOPNumber(self, frame, threshold, kernel, iterator):
+        #kernel1 = np.ones((kernel, kernel), np.uint8) #4
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        ret, thresh1 = cv2.threshold(gray, threshold, 255, cv2.THRESH_TOZERO)
-        thresh1 = cv2.dilate(thresh1, kernel1, iterations=iterator)
+        ret, thresh1 = cv2.threshold(gray, 190, 255, cv2.THRESH_TOZERO) #179
+        #thresh1 = cv2.dilate(thresh1, kernel1, iterations=1)
+        #ret, thresh1 = cv2.threshold(thresh1, iterator, 255, cv2.THRESH_BINARY)
+        return thresh1
+
+    def filterAndMorphOPStart(self, frame, threshold, kernel, iterator):
+        kernel1 = np.ones((kernel, kernel), np.uint8) 
+        frame2 = frame
+        farb = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        lower_blue = np.array([threshold, iterator, 188])
+        upper_blue = np.array([180, 255, 255])
+        mask = cv2.inRange(farb, lower_blue, upper_blue)
+        res = cv2.bitwise_and(frame2, frame2, mask=mask)
+        gray = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+
+
+        #ret, thresh1 = cv2.threshold(gray, threshold, 255, cv2.THRESH_TOZERO) #179
+        ret, thresh1 = cv2.threshold(gray, kernel, 255, cv2.THRESH_BINARY)
+        #thresh1 = cv2.erode(res, kernel1, iterations=1)
         return thresh1
 
     def camWorker(self):
@@ -72,46 +87,85 @@ class PlateDetection:
             except:
                 pass
 
-    def showWebcam(self):
-
+    def showCamNumber(self):
         cv2.namedWindow(self.windowOriginalFrame)
         self.createTrackbar()
 
         while(True):
             ret, frame = self.cap.read()
+            frame = cv2.flip(frame,-1)
             orignFram = frame
-            thresholdFrame = self.filterAndMorphOP(frame,self.getTrackbarValues(self.threshTrackbar),self.getTrackbarValues(self.kernelTrackbar),self.getTrackbarValues(self.iteratorTrackbar))
+            thresholdFrame = self.filterAndMorphOPNumber(frame, self.getTrackbarValues(self.threshTrackbar), self.getTrackbarValues(self.kernelTrackbar), self.getTrackbarValues(self.iteratorTrackbar))
 
-            contours, hierarchy = cv2.findContours(thresholdFrame,cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+            contours, hierarchy = cv2.findContours(thresholdFrame, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
             try:
                 for val in contours:
                     x, y, w, h = cv2.boundingRect(val)
-                    if ((h > self.getTrackbarValues(self.hMinTrackbar)) and (h < self.getTrackbarValues(self.hMaxTrackbar))):
+                    if ((h > 54 and (h < 74)) and (w > 24 and w < 50)):
                         cv2.rectangle(orignFram, (x, y), (x + w, y + h), (0, 255, 0), 2)
             except:
                 pass
 
-            cv2.imshow(self.windowTrackbar,thresholdFrame)
-            cv2.imshow(self.windowOriginalFrame,orignFram)
+            cv2.imshow(self.windowTrackbar, thresholdFrame)
+            cv2.imshow(self.windowOriginalFrame, orignFram)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
         self.cap.release()
 
+    def showCamStart(self):
+        cv2.namedWindow(self.windowOriginalFrame)
+        self.createTrackbar()
+
+        while(True):
+            ret, frame = self.cap.read()
+            frame = cv2.flip(frame,-1)
+            orignFram = frame
+            thresholdFrame = self.filterAndMorphOPStart(frame, self.getTrackbarValues(self.threshTrackbar), self.getTrackbarValues(self.kernelTrackbar), self.getTrackbarValues(self.iteratorTrackbar))
+
+
+            #contours, hierarchy = cv2.findContours(thresholdFrame, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            #try:
+            #    for val in contours:
+            #        x, y, w, h = cv2.boundingRect(val)
+            #        if ((h > self.getTrackbarValues(self.hMinTrackbar)) and (h < self.getTrackbarValues(self.hMaxTrackbar))): #hmax 105 hmin74
+            #            cv2.rectangle(orignFram, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            #except:
+            #    pass
+
+            cv2.imshow(self.windowTrackbar, thresholdFrame)
+            cv2.imshow(self.windowOriginalFrame, orignFram)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        self.cap.release()
+
+    def saveMovieFile(self):
+        forcc = cv2.VideoWriter_fourcc(*'XVID')
+        out = cv2.VideoWriter('output.avi',forcc,20.0,(640,480))
+        cv2.namedWindow(self.windowOriginalFrame)
+        self.createTrackbar()
+        while(True):
+            ret, frame = self.cap.read()
+            frame = cv2.flip(frame,-1)
+            out.write(frame)
+            cv2.imshow(self.windowTrackbar, frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                cap.release()
+                out.release()
+                cv2.destroyAllWindows()
+                break
+
 
 def main():
     plateDetection = PlateDetection()
     plateDetection.initCam()
     plateDetection.createTrackbar()
+    plateDetection.saveMovieFile()
 
-    camThread = Thread(target=plateDetection.camWorker)
-    filterThread = Thread(target=plateDetection.filterWorker)
-    camThread.start()
-    filterThread.start()
-    camThread.join()
-    filterThread.join()
-
-
-if __name__=='__main__':
+    
+if __name__ == '__main__':
     main()
