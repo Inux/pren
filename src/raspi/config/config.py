c1cf2d0e4b96ca2eda7dab7e2df0e0a39c881a19
inux <inux.steve@gmail.com>
2019-05-16 10:28:27 +0200

    distance calculated in config.py & adapted uart interface

--- src/raspi/config/config.py
+++ src/raspi/config/config.py
@@ -6,9 +6,9 @@ Configuration of the Raspberry
 
 # - speed values
 SPEED_CUBE_SEARCH = 10 #mm/s
-SPEED_MAXIMAL_LIMIT = 3500 #mm/s
-SPEED_NUMBER_DETECTION_LIMIT = 3500 #mm/s
-SPEED_STOP_SEARCH = 1000 #mm/s
+SPEED_MAXIMAL_LIMIT = 2200 #mm/s
+SPEED_NUMBER_DETECTION_LIMIT = 1500 #mm/s
+SPEED_STOP_SEARCH = 500 #mm/s
 
 # - crane poll time and waittime until finished
 CRANE_POLL_TIME = 0.1
@@ -17,6 +17,10 @@ CRANE_WAIT_TIME = 5
 # - distances of the parcour
 DISTANCE_ROUND_MM = 10000
 DISTANCE_STOP_MAX = 10000 # distance to last stop signal
+DISTANCE_UNTIL_ROUND_START = 500
+DISTANCE_ROUND_ONE = DISTANCE_UNTIL_ROUND_START + DISTANCE_ROUND_MM
+DISTANCE_ROUND_TWO = DISTANCE_ROUND_ONE + DISTANCE_ROUND_MM
+DISTANCE_UNTIL_STOP = DISTANCE_ROUND_TWO + DISTANCE_STOP_MAX
 
 
 # Component Parameters ----------------------------------------------
@@ -28,7 +32,7 @@ HB_INVALIDATE_TIME = 650 #ms
 
 # Tiny Config
 RESEND_TINY_MESSAGES = False
-MASTER_UART_INTERFACE_TINY = "/dev/ttyTiny"
+MASTER_UART_INTERFACE_TINY = "/dev/ttyAMA0"
 MASTER_UART_BAUD = 115200
 MASTER_UART_INTERFACE_PC = "/dev/ttys010" #Simulate on MacOs -> 'socat -d -d pty,raw,echo=0 pty,raw,echo=0' then 'screen /dev/ttys005'
 
